5400760b7158a3ce6f9f41a62f82e47f797e4e95
Arthur Toledo Neto <arthur.neto@grupozelo.com>
2021-10-27 11:40:32 -0300

    Configure database and users endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { EndpointModule } from './api/endpoints/endpoint.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EndpointModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
